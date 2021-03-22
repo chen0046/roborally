e1135479f6c74a5f1fe7ced74d191cb4d0f587eb
fiedler12 <o.lyngholm@gmail.com>
2021-03-22 09:42:25 +0100

    Javadoc

--- src/main/java/dk/dtu/compute/se/pisd/roborally/exceptions/ImpossibleMoveException.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/exceptions/ImpossibleMoveException.java
@@ -4,6 +4,10 @@ import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.Player;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 
+/**
+ * This class extends the Exception class and is used to throw an catch exceptions in situations where we are unable to move the the given Space
+ * In that we throw this exception to state that this move is impossible.
+ */
 public class ImpossibleMoveException extends Exception {
     private String message;
     private Player player;
