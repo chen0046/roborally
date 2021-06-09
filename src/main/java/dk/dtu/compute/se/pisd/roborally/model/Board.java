08daf855550e914fac471a1f8372a51d7b1e8437
Andreas <s205450@student.dtu.dk>
2021-06-09 13:03:23 +0200

    Rettet bug

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
@@ -51,8 +51,7 @@ public class Board extends Subject {
 
     List<ConveyorBelt> conveyorBelts = new ArrayList<>();
 
-    public Board(int width, int height, @NotNull String boardName) {
-        this.boardName = boardName;
+    public Board(int width, int height) {
         this.width = width;
         this.height = height;
         spaces = new Space[width][height];
@@ -66,14 +65,7 @@ public class Board extends Subject {
         spaces[1][0].setStart(0);
         spaces[2][0].setStart(0);
         spaces[3][0].setStart(0);*/
-        spaces[5][3].setHole(1);
-        spaces[7][7].setCheckpoint(1);
-        spaces[5][6].setCheckpoint(5);
-        spaces[4][4].setCheckpoint(2);
-        spaces[4][5].setCheckpoint(3);
-        spaces[4][2].walls.add(Heading.NORTH);
-        spaces[4][2].walls.add(Heading.EAST);
-        conveyorBelts.add(new ConveyorBelt(4, Heading.SOUTH, 6, 5));
+
         //spilleplade 1
         spaces[2][3].setHole(1);
         spaces[7][5].setHole(1);
@@ -246,7 +238,7 @@ public class Board extends Subject {
 
 
     public String getStatusMessage() {
-        return "Player = " + getCurrentPlayer().getName() + ", number of moves: " + getCount() + ", phase: " + getPhase().name() +
+        return "Player = " + getCurrentPlayer().getName() + ", Checkpoints Reached: " + getCurrentPlayer().checkpointsReached + ", number of moves: " + getCount() + ", phase: " + getPhase().name() +
                 ", Step: " + getStep();
     }
 
